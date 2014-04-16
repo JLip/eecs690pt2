b0edfd245fe74eefa80f0ec6ff178cd0d89ea5af
Adam <AtomDrake@gmail.com>
2014-04-16 10:23:38 -0500

    Updates to new appointment screen
    Starting to fill out the new appointment screen

--- PBPC/src/New_Appointment.java
+++ PBPC/src/New_Appointment.java
@@ -1,17 +1,26 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+
 import java.awt.Color;
 import java.awt.Toolkit;
+
 import javax.swing.JButton;
+
+import com.toedter.calendar.JDateChooser;
+
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Locale;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class New_Appointment {
 
 	private JFrame frmNewAppointment;
+	private JTextField timefld;
 
 	/**
 	 * Launch the application.
@@ -57,6 +66,15 @@ public class New_Appointment {
 		btnBackToAppointments.setBounds(25, 510, 243, 41);
 		frmNewAppointment.getContentPane().add(btnBackToAppointments);
 		
+		
+		JDateChooser AppointmentDate = new JDateChooser();
+		AppointmentDate.setToolTipText("Appointment Date");
+		AppointmentDate.setBounds(189, 27, 130, 33);
+		frmNewAppointment.getContentPane().add(AppointmentDate);
+		AppointmentDate.setLocale(Locale.US);
+		
+		
+		
 		JButton btnLogAppointment = new JButton("Log Appointment");
 		btnLogAppointment.addMouseListener(new MouseAdapter() {
 			@Override
@@ -66,9 +84,23 @@ public class New_Appointment {
 		btnLogAppointment.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnLogAppointment.setBounds(513, 510, 243, 41);
 		frmNewAppointment.getContentPane().add(btnLogAppointment);
+		
+		JLabel lblNewLabel = new JLabel("Appointment Date:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(25, 27, 154, 33);
+		frmNewAppointment.getContentPane().add(lblNewLabel);
+		
+		JLabel lblAppointmentTime = new JLabel("Appointment Time:");
+		lblAppointmentTime.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblAppointmentTime.setBounds(340, 27, 154, 33);
+		frmNewAppointment.getContentPane().add(lblAppointmentTime);
+		
+		timefld = new JTextField();
+		timefld.setBounds(492, 27, 124, 33);
+		frmNewAppointment.getContentPane().add(timefld);
+		timefld.setColumns(10);
 		frmNewAppointment.setBackground(Color.WHITE);
 		frmNewAppointment.setBounds(100, 100, 800, 600);
 		frmNewAppointment.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }

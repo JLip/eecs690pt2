445311be17c4beebc0c7ec0498b1808c532b5735
talamb <talamb.cs@gmail.com>
2014-04-24 00:39:18 -0500

    New App Main Functionality
    Almost all of it is work still needs a little work, but the main
    functionality works.

--- PBPC/src/New_Appointment.java
+++ PBPC/src/New_Appointment.java
@@ -12,11 +12,14 @@ import com.toedter.calendar.JDateChooser;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Vector;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JScrollPane;
@@ -29,6 +32,10 @@ import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
 import javax.swing.ListSelectionModel;
 import javax.swing.AbstractListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SpinnerListModel;
 
 
 public class New_Appointment {
@@ -43,6 +50,10 @@ public class New_Appointment {
 	@SuppressWarnings("rawtypes")
 	private JList txtApps;
 	public Vector<appItem> apps = new Vector<appItem>();
+	public Vector<petItem> pets = new Vector<petItem>();
+	
+	@SuppressWarnings("rawtypes")
+	public JList petList;
 	/**
 	 * Launch the application.
 	 */
@@ -101,16 +112,21 @@ public class New_Appointment {
 		
 		JLabel lblNewLabel_1 = new JLabel("Appoints That Day:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(25, 128, 141, 33);
+		lblNewLabel_1.setBounds(25, 270, 141, 33);
 		frmNewAppointment.getContentPane().add(lblNewLabel_1);
 		
 		
 		
-		JButton btnLogAppointment = new JButton("Log Appointment");
+		final JButton btnLogAppointment = new JButton("Log Appointment");
+		btnLogAppointment.setEnabled(false);
 		btnLogAppointment.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				AddAppToCal();
+				try {
+					AddAppToCal();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 		
@@ -152,7 +168,7 @@ public class New_Appointment {
 		AppointmentDate.setLocale(Locale.US);
 		
 		JScrollPane appScroll = new JScrollPane();
-		appScroll.setBounds(25, 172, 731, 310);
+		appScroll.setBounds(25, 314, 731, 168);
 		frmNewAppointment.getContentPane().add(appScroll);
 		
 		txtApps = new JList();
@@ -161,12 +177,29 @@ public class New_Appointment {
 				return apps.size();
 			}
 			public Object getElementAt(int index) {
-				String appInfo = printInfo(apps.get(index));
+				String appInfo = "ERROR";
+				try {
+					appInfo = printInfo(apps.get(index));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 				return appInfo;
 			}
-			private String printInfo(appItem appItem) {
-				// TODO Print line of appItem info
-				return null;
+			private String printInfo(appItem appItem) throws SQLException {
+				//Print line of appItem info
+				int	hours = appItem.StartTime / 100;
+				int minutes = appItem.StartTime % 100;
+				String name = "N/A";
+				String commandText = "SELECT Name FROM PetRecord WHERE PetID ="+ appItem.ID + ";";
+				ResultSet rs = SQL.ExecuteResultSet(commandText);
+				
+				if(rs.next()){
+					name = rs.getString("Name");
+					if(rs.wasNull()) name = "N/A";
+				}
+				
+				String finale = "Room: " + parseRoom(appItem.ExamRoom) + " " + hours + ":" + minutes + " for " + name;
+				return finale;
 			}
 		});
 		txtApps.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -195,20 +228,98 @@ public class New_Appointment {
 		AppointmentDate.setMinSelectableDate(new Date());
 		
 		serviceSpin = new JSpinner();
+		serviceSpin.setModel(new SpinnerListModel(new String[] {"Check Up", "Surgery"}));
 		serviceSpin.setBounds(483, 69, 207, 26);
 		frmNewAppointment.getContentPane().add(serviceSpin);
 		
 		roomSpin = new JSpinner();
+		roomSpin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		roomSpin.setModel(new SpinnerListModel(new String[] {"O.R.", "Exam Room 1", "Exam Room 2"}));
 		roomSpin.setBounds(82, 71, 207, 26);
 		frmNewAppointment.getContentPane().add(roomSpin);
+		
+		JLabel lblNewLabel_4 = new JLabel("Pets:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_4.setBounds(25, 101, 47, 26);
+		frmNewAppointment.getContentPane().add(lblNewLabel_4);
+		
+		JScrollPane petScroll = new JScrollPane();
+		petScroll.setBounds(25, 138, 731, 132);
+		frmNewAppointment.getContentPane().add(petScroll);
+		
+		petList = new JList();
+		petList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+				btnLogAppointment.setEnabled(true);
+			}
+		});
+		petScroll.setViewportView(petList);
+		createPetList(Main_Menu.OwnerID);
+		petList.setModel(new AbstractListModel() {
+			public int getSize() {
+				return pets.size();
+		}
+			public Object getElementAt(final int index) {
+				final petItem tempItem = pets.get(index);
+				final String tempName = tempItem.Name;
+				return tempName;
+		}
+	});
+		petList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		petScroll.setViewportView(petList);
 	}
 
 
 
-	protected void AddAppToCal() {
+	protected String parseRoom(int examRoom) {
+		String parsed = "";
+		if(examRoom == 1){
+			parsed = "Exam 1";
+		}
+		else if(examRoom == 2){
+			parsed = "Exam 2";
+		}
+		else{
+			parsed = "O.R.";
+		}
+		return parsed;
+	}
+
+
+
+	protected void AddAppToCal() throws SQLException {
 		if(CheckFieldsFilled()){
-			//TODO Add appointment to calendar
-			getAppsForDate(AppointmentDate.getDate());
+			int tme = Integer.parseInt(txtTime.getText());
+			int ownerID = -1;
+			petItem tempPet = pets.get(petList.getSelectedIndex());
+			java.sql.Date convDate = new java.sql.Date(AppointmentDate.getDate().getTime());
+			if(tme >= 0 &&  tme < 2400){
+			//Add appointment to calendar
+				String commandText = "SELECT * FROM Appointments WHERE Date='"+convDate +"' AND StartTime BETWEEN "+
+							(tme - 100) + " AND " + (tme + 100) + ";";
+				ResultSet rs = SQL.ExecuteResultSet(commandText);
+				
+				if(rs.next()){
+					//TODO time must be an hour apart
+				}
+				else{
+					commandText = "SELECT OwnerID FROM PetRecord WHERE PetID ="+pets.get(petList.getSelectedIndex()).ID +";";
+					rs = SQL.ExecuteResultSet(commandText);
+					if(rs.next()){
+						ownerID = rs.getInt("OwnerID");
+						commandText = "INSERT INTO Appointments" 
+									+"(PetID, StartTime, OwnerID, ExamRoom, Date, Services) VALUES "
+									+"("+tempPet.ID +", "+txtTime.getText()+", "+ ownerID+ ", "+ parseRoomInt() +", '"+convDate 
+									+"', '" + (String)serviceSpin.getValue()+"')"+";";
+						SQL.UpdateResultSet(commandText);
+					}
+				}
+				
+				getAppsForDate(AppointmentDate.getDate());
+			}
+			else{
+				//TODO create time range error message
+			}
 		}
 		else{
 			//TODO create error message;
@@ -216,18 +327,87 @@ public class New_Appointment {
 	}
 
 
+	private int parseRoomInt() {
+		int selected = 0;
+		String selString = (String)roomSpin.getValue();
+		if(selString.contains("1")){
+			selected = 1;
+		}
+		else if(selString.contains("2")){
+			selected = 2;
+		}
+		else{
+			selected = 3;
+		}
+		
+		return selected;
+	}
+
+
+
+	protected void createPetList(int ID) {
+		//select all pets names and ID and create pet list
+		String commandStr = "SELECT PetID, Name FROM PetRecord ORDER BY Name;";
+		Connection.Connect();
+		
+		try {
+		ResultSet rs = SQL.ExecuteResultSet(commandStr);
+		
+		//Iterate through the results and populate pet's tempList
+		while ( rs.next() ) {
+			String tempName = rs.getString("Name");
+			int tempID = rs.getInt("PetID");
+			petItem tempItem = new petItem(tempName, tempID);
+			pets.add(tempItem);
+		}
+		rs.close();		
+		
+		} catch ( Exception e ) {
+		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		JOptionPane.showMessageDialog(null, e.getClass().getName() + ": " + e.getMessage() );	
+		e.printStackTrace();
+		//System.exit(0);
+		}
+	}
 
 	private boolean CheckFieldsFilled() {
-		// TODO make sure all fields are filled.
+		//make sure all fields are filled
+		boolean filled = true;
 		
-		return false;
+		if(txtTime.getText().isEmpty() || AppointmentDate.getDate() == null) filled = false;
+		
+		return filled;
 	}
 
 
 
 	protected void getAppsForDate(Date date) {
 		if(date != null){
-			//TODO get Appointments for that date and update txtApps
+			Connection.Connect();
+			int ER = -1;
+			int ST = -1;
+			int id = -1;
+			java.sql.Date convDate = new java.sql.Date(date.getTime());
+			String commandStr = "SELECT * FROM Appointments WHERE Date='"+ convDate +"' ORDER BY StartTime;";
+
+		    try {
+		        ResultSet rs = SQL.ExecuteResultSet(commandStr);
+
+		        while ( rs.next() ) {
+		        	ER = rs.getInt("ExamRoom");
+		        	ST = rs.getInt("StartTime");
+		        	id = rs.getInt("PetID");
+		        	appItem tempItem = new appItem(ER, ST, id, date);
+		        	apps.add(tempItem);
+		        }
+		        rs.close();
+		        
+		      } catch ( Exception e ) {
+		        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		        JOptionPane.showMessageDialog(null,  e.getClass().getName() + ": " + e.getMessage() );	
+		        e.printStackTrace();
+		        //System.exit(0);
+		      }
 		}
 		else {
 			//TODO set txtApps to empty
@@ -237,7 +417,23 @@ public class New_Appointment {
 
 
 	protected void removeSelectedApp() {
-		// TODO Auto-generated method stub
+		appItem tempApp = apps.get(txtApps.getSelectedIndex());
+		java.sql.Date convDate = new java.sql.Date(tempApp.AppDate.getTime());
+		String commandText = "DELETE FROM Appointments WHERE PetID=" +tempApp.ID +", StartTime=" 
+				+ tempApp.StartTime + ", ExamRoom=" + tempApp.ExamRoom + ", Date='"+ convDate+"';";
+		SQL.ExecuteQuery(commandText);
+		
+		apps.clear();
+		getAppsForDate(tempApp.AppDate);
 		
 	}
+	
+	public class petItem{
+		public String Name = "";
+		public int ID = -1; //pet's ID
+			public petItem(String n, int i){
+			Name = n;
+			ID = i;
+			}
+		}
 }
